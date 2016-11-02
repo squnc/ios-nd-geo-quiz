32cf34f09102e039abb1c3a8de538e2acefeb4d5
pherzig <philipp.herzig@switzerland.com>
2016-11-02 18:23:14 +0100

    Added code to display an alert.

--- GeoQuiz/QuizViewController.swift
+++ GeoQuiz/QuizViewController.swift
@@ -36,6 +36,11 @@ class QuizViewController: UIViewController {
     // This function is called when user presses a flag button.
     @IBAction func flagButtonPressed(sender: UIButton) {
 
-        // TODO: Add code to display a message to the user telling them whether or not they guessed correctly.
+        if sender.tag == correctButtonTag {
+            displayAlert("Yes, thats right!", messageText: "Very good!")
+        } else {
+            displayAlert("Oh no", messageText: "That's wrong. Try again!")
+        }
+        
     }
 }
